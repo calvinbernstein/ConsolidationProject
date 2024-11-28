b0b32c6a82ecf1901db0b589024d1e4ad500565b
Calvin Bernstein <calvinbernstein@gmail.com>
2024-11-27 22:27:34 -0600

    Moved some docstrings to README, polished diceRoll function and created a dictionary to store player info

---
+++ README.md
@@ -0,0 +1,52 @@
+# tupleOut: *a game by Calvin Bernstein*
+
+**tupleOut is a simple dice game designed for multiple players (two for this version). The program will walk players through the game and prompt them to make choices. When the game is over, tupleOut will create a "results" file or append to an existing one with the results of the game.**
+
+### How It Works:
+- The object of the game is to score the most points, or to be the first to reach a certain score.
+- Players take turns rolling dice to score points, as described below.
+- Each turn, the active player rolls three dice:
+	- If all three dice are rolled with the same number, the player has “tupled out”, and ends their turn with zero points. (For example, rolling three “4”s at the same time.)
+	- If two dice have the same value, they are “fixed”, and they cannot be re-rolled.
+	- During their turn, the player can re-roll any dice that are not “fixed”, as often as they would like, until they decide to stop, or until they “tuple out” (get three of the same number).
+- When a player decides to stop, they score points equal to the total of the three dice, and then
+their turn ends.
+- If a player “tuples out”, their turn ends and they score 0 points for that turn.
+
+##### *Example Scenarios*
+1. A player starts out by rolling a 1, 2, and 2 on three dice.
+	- The 2’s are fixed, because there’s two of them, so they can’t re-roll those.
+	- But they want to reroll the 1, since it’s so low.
+	- They re-roll it to a 3, but they want to re-roll it again, to try to get higher.
+	- When they re-roll the second time, it turns up as a 2, and since the other two dice are also 2’s, they have “tupled out”. They get zero points for the round.
+
+2. A player starts by rolling a 1, 2, and 4.
+	- They decide to roll all three again, and they get a 1 and two 5’s. This means the two 5’s are fixed, and they could choose to re-roll the 1.
+	- However, they decide to stop here, because the 5’s are high, and they don’t want to risk “tupling out” by re-rolling the 1 and getting a third 5.
+	- So they stop their turn with 11 points (5 + 5 + 1).
+
+
+### Additional Questions and Future Plans
+
+**When is the game over?**
+    The game ends after five rounds.
+
+**How and when will you display the running scores, so that players know what the current scores are?**
+    The scores will be displayed after each round.
+    
+
+**Can the game record the scores for each game, including who won?**
+    The game will record the scores for each game at the end, including the final scores and the winner between the players.
+
+**Can the game record something like “high score” records over many games, or a running tally of how many games a particular player has won?**
+    This feature will be added in a future version of the game, probably by storing the data in a CSV file.
+
+**Would the game be better or more interesting if the dice were changed, including the number of dice or the number of values on each die?**
+    The program has been designed with customization in mind. Many functions can be easily modified to change the number of dice rolled, the number of values on each die, and the values themselves.
+
+**What about adding rules for additional special scoring?**
+    There are currently no plans for additional special scoring, but it may be added in a future version.
+
+**How do I start the game?**
+Place the python file in a directory where you are comfortable with a results file being created. From there, navigate to the directory in your terminal and run the program using `python tupleOut.py`. Everything is straightforward after that!
+	
--- tupleOut.py
+++ tupleOut.py
@@ -1,79 +1,87 @@
-"""
-- Each turn, the active player rolls three dice
+# tupleOut: a game by Calvin Bernstein
+# Please see the README file for information on how to play.
 
-- If all three dice are rolled with the same number, the player has “tupled out”, and ends their turn with zero points. (For example, rolling three “4”s at the same time.)
-
-- If two dice have the same value, they are “fixed”, and they cannot be re-rolled.
-
-- During their turn, the player can re-roll any dice that are not “fixed”, as often as they would like, until they decide to stop, or until they “tuple out” (get three of the same number).
-
-- When a player decides to stop, they score points equal to the total of the three dice, and then
-"""
-
+# Import required libraries
 import random
 
+"""
+diceRoll Function
+For each "roll" (specified by the diceQuantity parameter), the diceRoll function selects a random item from the dieOptions list contained within it and appends the item to a list which is returned after all of the dice have been rolled.
 
-# 2 players
-# five total turns
-# display the scores after each round
-# record the scores to a text file
-# txt file can store the high scores
-# computer player? 
-# additional special scoring? different dice?
-
-# Dice roll funciton
-def diceRoll(diceQuantity):
-    # Options for the sides of the die, can be changed later for special dice
+PARAMETERS:
+    diceQuantity (optional) - Use this parameter to specify how many dice rolls should be returned. If left blank, the function will roll one die.
+"""
+def diceRoll(diceQuantity = -1):
+    # Options for the sides of the die, can be modified for special dice
     dieOptions = [1, 2, 3, 4, 5, 6]
     
-    diceResults = []
+    # List storing dice rolls to be returned
+    result = []
     
-    for i in range(0, diceQuantity):
-        diceRoll = random.choice(dieOptions)
-        diceResults.append(diceRoll)
-        
-    return diceResults
+    if diceQuantity > 0:
+        for i in range(0, diceQuantity):
+            diceRoll = random.choice(dieOptions)
+            result.append(diceRoll)
+    elif diceQuantity == -1:
+        result.append(random.choice(dieOptions))
+    else:
+        raise Exception("diceRoll Function Error - A dice roll was called for with an invalid quantity.")
+    return result
 
+def roundUI(round, playerID)
 
 # /// CONFIGURATION VARIABLES
-# Variable for tracking the current round (out of 3)
-currentRound = 0
+currentRound = 0    # Variable for tracking the current round (out of 3)
+maxRounds = 5       # Maximum rounds for the game
+diceUsed = 3        # How many dice are rolled for each turn
 
-# Maximum rounds for the game
-maxRounds = 3
-
-# How many dice are used in the game
-diceUsed = 3
-
-
-# FUNCITON TO RETERN PLAYER'S TURN
+# Dictionary containing player information
+playerInfo = {
+    "playerOneID": 1,       # Player ID for Player One
+    "playerOneDice": [],    # List for storing Player One's matching dice
+    "playerOnePoints": 0,   # Variable to track Player One's score
+    
+    "playerTwoID": 1,       # Player ID for Player Two
+    "playerTwoDice": [],    # List for storing Player Two's matching dice
+    "playerTwoPoints":, 0   # Variable to track Player Two's score
+}
 
 # Function to check for different scenarios - tuple out, two matching, etc...
+"""
+checkRoll Function
+This function analyzes a list of dice for common scenarios which trigger an event in the game.
+More specifically, the function checks for matching dice and "tupling out".
+
+"""
 def checkRoll(dice):
     tupleOut = all(i == list[0] for i in list)
     if tupleOut == True:
         return "tupleOut"
     
     # If 3 dice match...
-    
-# general scorekeeping function - store scores, return the scoreboard, other features
-def scorer():
-    
 
 
+"""
+This variable will keep track of whether the game should continue running or not.
+If the maximum number of rounds is reached or a player "tuples out", the gameActive variable will be set to False.
+"""
+gameActive = True
+
+# Introductory message reminding the user to check the README if they haven't already
 print("Welcome to TupleOut! Make sure to read the README for the rules.")
-for i in range(0, maxRounds):
-    
+
+while gameActive == True:
     
     if currentRound == 0:
-        playerSelection = input("PLAYER 1: It's your turn. Type ROLL to roll the dice.")
+        playerSelection = input("PLAYER 1: It's your turn. Type ROLL to roll the dice.\n")
     elif currentRound > 0:
-        playerSelection = input("PLAYER 1: It's your turn. Would you like to ROLL or PASS?")
+        playerSelection = input("PLAYER 1: It's your turn. Would you like to ROLL or PASS?\n")
     
     currentRound += 1
     if playerSelection == "ROLL":
+        if 
         currentRoll = diceRoll(diceUsed)
-        print("Your roll: " + str(currentRoll))
+        print("Your roll: " + str(currentRoll)+"\n")
         
         
     
